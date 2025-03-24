aae182f0ad196e370bb77345d5a17d011f3b09a2
nishitasarkar <nishitasarkar35@gmail.com>
2025-03-24 20:43:49 +0600

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  /* cout << "your age";
+  int age;
+  cin >> age;
+  cout << "your age is "<< age << " years old " <<endl;
+
+  cout << "Enter first number" << endl;
+  int n1;
+  cin >> n1;
+  cout << "Enter second number" << endl;
+  int n2;
+  cin >> n2;
+  int sum=n1+n2;
+  cout << "The sum is " << sum ;*/
+  cout << "Enter the number " << endl;
+  int num;
+  cin >> num;
+  int squ=num*num;
+  cout << "The square of the number is " << squ;
+}
