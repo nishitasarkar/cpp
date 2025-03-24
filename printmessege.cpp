d0fb5ea875dc5240735ac92fe62311190e8f97af
nishitasarkar <nishitasarkar35@gmail.com>
2025-03-24 20:56:41 +0600

    Add files via upload

---
+++ printmessege.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter a message" << endl;
+    string message;
+    cin >> message;
+    cout << "The message is " << message;
+}
